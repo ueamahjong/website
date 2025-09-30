ea7476caef5ddcbcda534c4c0abe2a11c077ef9a
Zekiah-A <zekiahamoako@outlook.com>
2025-09-30 23:50:54 +0100

    More work on hero title logo, links and general design

--- src/main.js
+++ src/main.js
@@ -19,6 +19,11 @@ export const $$ = (selector) => {
 }
 
 const tileInfoDialog = $("#tileInfoDialog");
+tileInfoDialog.addEventListener("click", (event) => {
+	if (event.target === tileInfoDialog) {
+		tileInfoDialog.close();
+	}
+});
 
 function showTileInfoDialog(tileName, description) {
 	tileInfoDialog.showModal();
@@ -28,17 +33,9 @@ function showTileInfoDialog(tileName, description) {
 	tileDescriptionElement.textContent = description;
 }
 
-const tileFiveCoin = $("#tileFiveCoin");
-tileFiveCoin.addEventListener("click", () => {
-	showTileInfoDialog(
-		"5 Of Coins",
-		"The 5 of Coins is a special tile in Mahjong, often marked with a red dot. It is considered a 'dora' tile, which can increase the value of your hand if included."
-	);
-});
-
-const tileRedDragon = $("#tileRedDragon");
-tileRedDragon.addEventListener("click", () => {
-	showTileInfoDialog("Red Dragon", "The Red Dragon is one of the three dragon tiles in Mahjong. It is often associated with good fortune and is a valuable tile to complete certain hands.");
+const tileSixCharacters = $("#tileSixCharacters");
+tileSixCharacters.addEventListener("click", () => {
+	showTileInfoDialog("Six of Characters", "The Six of Characters is a tile in the character suit of Mahjong. It is part of the numbered tiles and can be used to form sequences or sets.");
 });
 
 const tileEastWind = $("#tileEastWind");
@@ -46,12 +43,18 @@ tileEastWind.addEventListener("click", () => {
 	showTileInfoDialog("East Wind", "The East Wind is one of the four wind tiles in Mahjong. It represents the east direction and can be significant in certain hands, especially if it matches the prevailing wind.");
 });
 
-const tileEightBamboo = $("#tileEightBamboo");
-tileEightBamboo.addEventListener("click", () => {
-	showTileInfoDialog("8 of Bamboo", "The 8 of Bamboo is a tile in the bamboo suit of Mahjong. It is part of the numbered tiles and can be used to form sequences or sets.");
+const tileEightBamboos = $("#tileEightBamboos");
+tileEightBamboos.addEventListener("click", () => {
+	showTileInfoDialog("8 of Bamboos", "The 8 of Bamboos is a tile in the bamboo suit of Mahjong. It is part of the numbered tiles and can be used to form sequences or sets.");
 });
 
 const closeDialogButton = $("#closeDialog");
 closeDialogButton.addEventListener("click", () => {
 	tileInfoDialog.close();
 });
+
+const tileFiveCoins = $("#tileFiveCoins");
+tileFiveCoins.addEventListener("click", () => {
+	showTileInfoDialog("5 Of Coins", "The 5 of Coins is a special tile in Mahjong, often marked with a red dot. It is considered a 'dora' tile, which can increase the value of your hand if included.");
+});
+
